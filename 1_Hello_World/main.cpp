eedb3a5c0e2cebefcc878407e902ba3cb63da0b3
Kumpei Shiraishi <kumpeishiraishi@gmail.com>
2023-02-27 10:16:13 +0900

    Bug fix.

--- 1_Hello_World/main.cpp
+++ 1_Hello_World/main.cpp
@@ -8,4 +8,7 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     std::cout << "Hello, I'm " << rank << " / " << size << std::endl;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
 }
